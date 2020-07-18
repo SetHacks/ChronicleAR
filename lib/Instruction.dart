4750506d76672418ded22409bddeccfccb850ae2
Jeannie Q <jeanniequach01@gmail.com>
2020-07-18 17:10:01 -0400

    Finshing touches on icons

--- lib/Instruction.dart
+++ lib/Instruction.dart
@@ -6,16 +6,37 @@ class Instruction extends StatelessWidget {
     return MaterialApp(
         home: Scaffold(
       backgroundColor: Color(0xFF2965FF),
+/*Row(
+  children: <Widget>[
+    Expanded(
+      child: Text('Deliver features faster', textAlign: TextAlign.center),
+    ),
+    Expanded(
+      child: Text('Craft beautiful UIs', textAlign: TextAlign.center),
+    ),
+    Expanded(
+      child: FittedBox(
+        fit: BoxFit.contain, // otherwise the logo will be tiny
+        child: const FlutterLogo(),
+      ),
+    ),
+  ],
+)
+ */
+
       body: Center(
         child: Container(
           child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 Container(
-                  child: Image(
-                    image: AssetImage('assets/PhoneScanning.png'),
-                    height: 150,
-                    width: 150,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(25),
+                    child: Image(
+                      image: AssetImage('assets/PhoneScanning.png'),
+                      height: 150,
+                      width: 150,
+                    ),
                   ),
                   alignment: Alignment(-1.0, -1.0),
                   padding: EdgeInsets.only(left: 8, bottom: 100),

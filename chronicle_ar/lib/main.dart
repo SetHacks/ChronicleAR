ce9c46b0bc37a54004abc3b5c04d17f67daf5fed
Nick Liu <nickliu02@gmail.com>
2020-07-18 11:27:29 -0400

    Added ar dependency

--- chronicle_ar/lib/main.dart
+++ chronicle_ar/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:arcore_flutter_plugin/arcore_flutter_plugin.dart';
 
-void main() {
+void main() => runApp(MaterialApp(
+  home: Scaffold(
 
-}
-
+  ),
+));

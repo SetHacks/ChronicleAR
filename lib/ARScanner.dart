aa5caf5dbbc3fc90791c0e8338bbff0895836dce
Nithin Muthukumar <kathrikai30201094@gmail.com>
2020-07-18 23:03:04 -0400

    kg

--- lib/ARScanner.dart
+++ lib/ARScanner.dart
@@ -54,7 +54,7 @@ class ARScannerPage extends State<ARScanner>{
           _imageFile = image;
           getBookInfo().then((value){
             if(value!=null){
-              
+
 
             }
 
@@ -86,60 +86,6 @@ class ARScannerPage extends State<ARScanner>{
     );
   }
 
-  void _onArCoreViewCreated(ArCoreController controller) {
-    arCoreController = controller;
-    _addSphere(arCoreController);
-    _addCylindre(arCoreController);
-    _addCube(arCoreController);
-  }
-
-  void _addSphere(ArCoreController controller) {
-    final material = ArCoreMaterial(
-        color: Color.fromARGB(120, 66, 134, 244));
-    final sphere = ArCoreSphere(
-      materials: [material],
-      radius: 0.1,
-    );
-    final node = ArCoreNode(
-      shape: sphere,
-      position: vector.Vector3(0, 0, -1.5),
-    );
-    controller.addArCoreNode(node);
-
-  }
-
-  void _addCylindre(ArCoreController controller) {
-    final material = ArCoreMaterial(
-      color: Colors.red,
-      reflectance: 1.0,
-    );
-    final cylindre = ArCoreCylinder(
-      materials: [material],
-      radius: 0.5,
-      height: 0.3,
-    );
-    final node = ArCoreNode(
-      shape: cylindre,
-      position: vector.Vector3(0.0, -0.5, -2.0),
-    );
-    controller.addArCoreNode(node);
-  }
-
-  void _addCube(ArCoreController controller) {
-    final material = ArCoreMaterial(
-      color: Color.fromARGB(120, 66, 134, 244),
-      metallic: 1.0,
-    );
-    final cube = ArCoreCube(
-      materials: [material],
-      size: vector.Vector3(0.5, 0.5, 0.5),
-    );
-    final node = ArCoreNode(
-      shape: cube,
-      position: vector.Vector3(-0.5, 0.5, -3.5),
-    );
-    controller.addArCoreNode(node);
-  }
 
   @override
   void dispose() {
---
+++ lib/CapturePage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CapturePage extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() => new CapturePageState();
+
+}
+
+class CapturePageState extends State<CapturePage>{
+  @override
+  Widget build(BuildContext context) {
+
+  }
+}

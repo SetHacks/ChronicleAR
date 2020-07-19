018afe8b9a4a6596f717f09ff3aaa708dbd1e5ef
Nithin Muthukumar <kathrikai30201094@gmail.com>
2020-07-19 03:14:24 -0400

    m

--- lib/graphQLConf.dart
+++ lib/graphQLConf.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 
 class GraphQLConfiguration {
-  static HttpLink httpLink = HttpLink(uri: 'localhost:3000');
+  static HttpLink httpLink = HttpLink(uri: '134.209.168.108:3000/graphql');
 
   static ValueNotifier<GraphQLClient> client = ValueNotifier(
     GraphQLClient(

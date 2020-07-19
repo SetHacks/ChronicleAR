0f013a641389c1131b1ae3ffbcab94ea7c5925dd
Jeannie Q <jeanniequach01@gmail.com>
2020-07-19 01:16:21 -0400

    Creation of new slider file

---
+++ lib/Slider.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class Slider extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
